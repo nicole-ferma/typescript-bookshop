f55f897eda185d381318c3dcda845c45dde3ddd1
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-09-10 15:48:12 +1200

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+import { About } from './pages/About'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Hi</h1>
+    <Container className="mb-4">
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/store' element={<Store />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+    </Container>
   )
 }
 
