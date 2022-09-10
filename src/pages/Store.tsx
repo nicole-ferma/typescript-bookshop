3f7279faca730b04b40eb310349e2bb17bfef68d
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-09-10 16:43:36 +1200

    create books json file

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,15 @@
+import { Col, Row } from "react-bootstrap"
+import books from "../data/books.json"
+
 export function Store() {
-  return <h1>Store</h1>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {books.map(book => (
+          <Col>{JSON.stringify(book)}</Col>
+        ))}
+      </Row>
+    </>
+  )
 }
