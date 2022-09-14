7a5d0601d9e9a8df85030f6fc37ffe6efd4ce6bf
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-09-15 11:44:56 +1200

    set cart types

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,16 +1,29 @@
-import { createContext, ReactNode, useContext } from "react"
+import { createContext, ReactNode, useContext, useState } from "react"
 
 type CartProviderProps = {
   children: ReactNode
 }
 
-const CartContext = createContext({})
+type CartItem = {
+  id: number
+  quantity: number
+}
+
+type CartContext = {
+  getItemQuantity: (id: number) => number
+  increaseCartQuantity: (id: number) => void
+  decreaseCartQuantity: (id: number) => void
+  removeFromCart: (id: number) => void
+} 
+
+const CartContext = createContext({} as CartContext)
 
 export function useCart() {
   return useContext(CartContext)
 }
 
 export function CartProvider( { children }: CartProviderProps ) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
   return (
     <CartContext.Provider value={{}}>
       {children}
