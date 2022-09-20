26f2907058b8b8bf1bbf1a4bf4d462d3cf6cccc7
mangobluebird <103153488+nicole-ferma@users.noreply.github.com>
2022-09-21 10:49:46 +1200

    create openCart and closeCart functions

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -10,10 +10,14 @@ type CartItem = {
 }
 
 type CartContext = {
+  openCart: () => void
+  closeCart: () => void
   getItemQuantity: (id: number) => number
   increaseCartQuantity: (id: number) => void
   decreaseCartQuantity: (id: number) => void
   removeFromCart: (id: number) => void
+  cartQuantity: number
+  cartItems: CartItem[]
 } 
 
 const CartContext = createContext({} as CartContext)
@@ -23,7 +27,16 @@ export function useCart() {
 }
 
 export function CartProvider( { children }: CartProviderProps ) {
+  const [isOpen, setIsOpen] = useState(false)
   const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  )
+
+  const openCart = () => setIsOpen(true)
+  const closeCart = () => setIsOpen(false)
 
   function getItemQuantity(id: number) {
     // if item exists, return its quantity value
@@ -68,8 +81,9 @@ export function CartProvider( { children }: CartProviderProps ) {
     })
   }
 
+
   return (
-    <CartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart }}>
+    <CartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, openCart, closeCart, cartItems, cartQuantity }}>
       {children}
     </CartContext.Provider>
   )
